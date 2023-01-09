23dbfe3c91d83724122cf2dc0e3a845befc263d5
Michael Shirazi <96745467+shirazim@users.noreply.github.com>
2023-01-08 17:42:50 -0800

    Update
    Cleaned up the script and updated some of the logic

--- TICKET MINUTE CONVERTER.ps1
+++ TICKET MINUTE CONVERTER.ps1
@@ -1,79 +1,111 @@
-<#
-This was made to convert strings that look like: "4 days 3 hours 30 minutes" into minutes which would be 5970. 
-#>
+$Tickerrs = import-csv 'tickets.csv'
 
-$tickets = import-csv C:\Users\michaelshirazi\Downloads\AllTickets.csv
-
-foreach ($t in $tickets){
-
+foreach ($t in $Tickerrs){
                                                             #ONLY DAY 
     if($t.calendar_stc -match "day" -and $t.calendar_stc -notmatch "hour" -and $t.calendar_stc -notmatch "minute"){
-    [int]$day = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').trim(',') 
+    [int]$day = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').trim(',')
     
-    $t.calendar_stc
+    #$t.calendar_stc
     $final = (($day * 24) * 60)
-    Write-Host $final
+    $ticketnumber = $t.number
+    $description = $t.short_description.Replace(',','')
+  
+        [PSCustomObject]@{
+          INC = $t.number
+          Time = $t.calendar_stc
+          Minutes = $final
+       } 
+    #| out-file $newcsv -Append
 
     }   
-                                                           #DAY + HOUR
+                                                        #DAY + HOUR
     elseif($t.calendar_stc -match "day" -and $t.calendar_stc -match "hour" -and $t.calendar_stc -notmatch "minute"){
         $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',')  
         $days = [int]$num[0]
         $hours = [int]$num[1]
-        $t.calendar_stc
+    
         $final = (($days * 24)*60) + ($hours * 60)
-	$final
+        [PSCustomObject]@{
+          INC = $t.number
+          Time = $t.calendar_stc
+          Minutes = $final
+       } 
+   
+       #| out-file $newcsv -Append
     }
                                                           #DAY + MINUTES
       elseif($t.calendar_stc -match "day" -and $t.calendar_stc -notmatch "hour" -and $t.calendar_stc -match "minute"){
     $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').trim(',').split(',') 
-    $t.calendar_stc
     $day = [int]$num[0]
     $hours = [int]$num[1] 
     $final = (($day * 24)*60) + ($minute)
-    $final
-    } 
-         
+    
+    [PSCustomObject]@{
+      INC = $t.number
+      Time = $t.calendar_stc
+      Minutes = $final
+   } #| out-file $newcsv -Append
+
+    }        
                                                        #DAY + HOUR + MINUTES
       elseif($t.calendar_stc -match "day" -and $t.calendar_stc -match "hour" -and $t.calendar_stc -match "minute"){
           $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',')  
           $days = [int]$num[0]
           $hours = [int]$num[1]
-          $minute = [int]$num[2]
-          
-          
-          $t.calendar_stc
+          $minute = [int]$num[2]         
           $final = (($days * 24)*60) + ($hours * 60) + ($minute)
-          $final
+          [PSCustomObject]@{
+            INC = $t.number
+            Time = $t.calendar_stc
+            Minutes = $final
+         } #| out-file $newcsv -Append
       }      
                                                            #HOURS + MINUTES
       elseif($t.calendar_stc -notmatch "day" -and $t.calendar_stc -match "hour" -and $t.calendar_stc -match "minute"){
-       $t.calendar_stc
        $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',')  
        $hours = [int]$num[0]
        $minute = [int]$num[1]
 
        $final = ($hours * 60) + ($minute)
-       $final 
+       [PSCustomObject]@{
+        INC = $t.number
+        Time = $t.calendar_stc
+        Minutes = $final
+     } #| out-file $newcsv -Append
+      
+      }
         
     
                                                           #ONLY HOURS
     if($t.calendar_stc -notmatch "day" -and $t.calendar_stc -match "hour" -and $t.calendar_stc -notmatch "minute"){
-    $t.calendar_stc
-    $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',')  
+    $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',') 
     $hours = [int]$num[0]
     $final = ($hours * 60)
-    $final
-   }
 
-              ### ONLY MINUTES
+    [PSCustomObject]@{
+      INC = $t.number
+      Time = $t.calendar_stc
+      Minutes = $final
+   } #| out-file $newcsv -Append
+
+   }
+                                                  ### ONLY MINUTES
 elseif($t.calendar_stc -notmatch "day" -and $t.calendar_stc -notmatch "hour" -and $t.calendar_stc -match "minute"){
-  $t.calendar_stc
   $num = ($t.calendar_stc -replace '[A-Z]','' -replace '   |  ',' ').Replace(' ',',').split(',')  
   $minute = [int]$num[0]
-  $minute
+  [PSCustomObject]@{
+    INC = $t.number
+    Time = $t.calendar_stc
+    Minutes = $minute
+ } # | out-file $newcsv -Append
  }
 
-
+ elseif(!($t.calendar_stc)){
+  <#
+  [PSCustomObject]@{
+    INC = $t.number
+    Time = $t.calendar_stc
+    Minutes = $final
+ } #| out-file $newcsv -Append #>
+ }
 }
-
